36a1be714244734145745260fdde90901ee7f504
samadon1 <calvinsamueldonkor@gmail.com>
2022-11-01 04:21:31 +0000

    make it better

--- base/models.py
+++ base/models.py
@@ -66,7 +66,7 @@ class Reviews(models.Model):
     )
     ngo = models.ForeignKey(Ngo, on_delete=models.CASCADE)
     name = models.TextField()
-    user_type = models.TextField(choices = USER_TYPE, default='Donor')
+    user_type = models.TextField(max_length=20,choices = USER_TYPE, default='Donor')
     date = models.DateTimeField(auto_now_add= True)
     comment = models.TextField()
     
@@ -362,7 +362,7 @@ class ClaimProfile(models.Model):
     ngo = models.ForeignKey(Ngo, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=200, null= False, blank = True)
     role = models.TextField(verbose_name="Briefly describe your role ", max_length=200, null= False, blank = True)
-    connection = models.CharField(verbose_name="How are you connected to the organization?", choices = CONNECTIONS, default='Staff')
+    connection = models.TextField(verbose_name="How are you connected to the organization?", max_length=20, choices = CONNECTIONS, default='Staff')
     phone = PhoneNumberField(max_length=40, null= False, blank = True)
     email_address = models.EmailField(max_length=254 , null= False, blank = True)
     organization_website = models.URLField(max_length=200, null= False, blank = True)
